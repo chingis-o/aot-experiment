7b5c90fe5e4b5c7dbbe8cf7de7b4d674051f4181
Chingis Ochirov <chingis09@gmail.com>
2025-04-12 17:27:19 +0800

    ollama chat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,18 @@
 import Head from "next/head";
+import { useState } from "react";
+import { ChatOllama } from "@langchain/ollama";
+import type { MessageContent } from "@langchain/core/messages";
+
+const llm = new ChatOllama({
+  model: "deepseek-r1:7b",
+  temperature: 0,
+});
 
 export default function Home() {
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState<MessageContent>();
+  const [loading, setLoading] = useState(false);
+
   return (
     <>
       <Head>
@@ -9,7 +21,33 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center">
-        Hello
+        <textarea
+          className="mb-6 rounded-md border-2 border-blue-300 px-4 py-1 outline focus:border-blue-400"
+          onChange={(event) => setPrompt(event.target.value)}
+        >
+          {prompt}
+        </textarea>
+        <button
+          className="cursor-pointer rounded-md border-2 border-blue-300 px-7 py-1"
+          disabled={loading}
+          onClick={async () => {
+            console.log("call");
+            setLoading(true);
+            setResult("");
+            try {
+              const result = await llm.invoke(prompt);
+
+              setResult(result.content);
+            } catch (error) {
+              console.log(error);
+            }
+            setLoading(false);
+          }}
+        >
+          Invoke
+        </button>
+        {loading ? "generating..." : ""}
+        <div>{String(result)}</div>
       </main>
     </>
   );
