a9d896db71882c4c35d952f0ea9e0b339faf5b9a
Chingis Ochirov <chingis09@gmail.com>
2025-04-18 19:51:16 +0800

    accordion example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
 import Head from "next/head";
 import { Button } from "@/components/ui/button";
 import Question from "~/components/Question";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 import gsm8k from "../data/gsm8k/test.json";
 
@@ -14,6 +20,14 @@ export default function Home() {
       </Head>
       <main className="my-10 flex min-h-screen flex-col items-center">
         <div className="container grid max-w-2/3 justify-start">
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>Is it accessible?</AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
           <ul className="my-5 flex justify-start gap-2.5">
             <li>
               <Button className="cursor-pointer">gsm8k</Button>
