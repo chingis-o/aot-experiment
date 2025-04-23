21c513852b1c8240e608d0f8b8abf1224a304280
Chingis Ochirov <chingis09@gmail.com>
2025-04-23 16:49:44 +0800

    dialog, input, label components added

--- src/components/GenerateResponse.tsx
+++ src/components/GenerateResponse.tsx
@@ -11,6 +11,17 @@ import {
 import Result from "./Result";
 import Cascade from "./Cascade";
 import { parseDag, type DAG } from "@/utils/parseDag";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 import prompts from "../prompts/examples";
 
@@ -35,8 +46,38 @@ export default function GenerateResponse({ question }: { question: string }) {
 
   return (
     <div className="container grid justify-items-start">
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline">Edit Profile</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you're done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <Input id="name" value="Pedro Duarte" className="col-span-3" />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Username
+              </Label>
+              <Input id="username" value="@peduarte" className="col-span-3" />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
       <Textarea
-        className="mb-6"
+        className="mb-3"
         rows={10}
         onChange={(event) => setPrompt(event.target.value)}
         value={prompt}
