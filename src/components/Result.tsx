35f34caf99ab2e33bb57e7c05a914e9ee48d4b82
Chingis Ochirov <chingis09@gmail.com>
2025-04-23 15:56:17 +0800

    accordion fix

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -3,7 +3,7 @@ import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@radix-ui/react-accordion";
+} from "@/components/ui/accordion";
 import React from "react";
 import { handleThinkTag } from "~/utils/handleThinkTag";
 
