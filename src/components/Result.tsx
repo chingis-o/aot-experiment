18705f71dd6cda5c59214000bf59b82a4b9e55fa
Chingis Ochirov <chingis09@gmail.com>
2025-04-24 17:59:08 +0800

    fix thinking text

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -5,9 +5,23 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import React from "react";
-import { handleThinkTag } from "~/utils/handleThinkTag";
 
-export default function Result({ result }: { result: string }) {
+export default function Result({
+  result: text,
+  loading,
+}: {
+  result: string;
+  loading: boolean;
+}) {
+  const splitPosition =
+    text.indexOf("</think>") > 0 ? text.indexOf("</think>") : text.length;
+
+  const thinking = text.substring(0, splitPosition - 1).replace("<think>", "");
+
+  const result = text
+    .substring(splitPosition, text.length)
+    .replace("</think>", "");
+
   return (
     <>
       <Accordion
@@ -18,14 +32,14 @@ export default function Result({ result }: { result: string }) {
       >
         <AccordionItem value="item-1">
           <AccordionTrigger className="mb-1.5 bg-[#f7f8fc] px-2.5 py-2 text-sm font-medium">
-            Thinking completed
+            {loading ? "Thinking process..." : "Thinking completed"}
           </AccordionTrigger>
-          <AccordionContent className="p-1.5 text-[#8f91a8]">
-            {handleThinkTag(result).thinking}
+          <AccordionContent className="p-1.5 whitespace-pre-line text-[#8f91a8]">
+            {thinking}
           </AccordionContent>
         </AccordionItem>
       </Accordion>
-      <div>{handleThinkTag(result).result}</div>
+      <div className="whitespace-pre-line">{result}</div>
     </>
   );
 }
--- src/utils/handleThinkTag.ts
+++
@@ -1,9 +0,0 @@
-export function handleThinkTag(text: string) {
-  const matchBetweenTags = text.match(/<think>(.*?)<\/think>/i);
-  const matchAfterTags = text.match(/<\/think>(.*)/i);
-
-  return {
-    thinking: matchBetweenTags ? matchBetweenTags[1] : "",
-    result: matchAfterTags ? matchAfterTags[1] : "",
-  };
-}
