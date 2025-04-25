ea1d59b92c94ddf95b78f300f7130b55fe92f5bd
Chingis Ochirov <chingis09@gmail.com>
2025-04-25 13:50:40 +0800

    fix key prop

--- src/components/Cascade.tsx
+++ src/components/Cascade.tsx
@@ -6,18 +6,18 @@ export default function Cascade({ chain }: { chain: Chain[] }) {
   return (
     <div className="my-3 w-full">
       <ul>
-        {chain.map((chain: Chain) => {
+        {chain.map((chain: Chain, index) => {
           return (
-            <div className="grid grid-cols-[20px_1fr] text-sm">
+            <div key={index} className="grid grid-cols-[20px_1fr] text-sm">
               <div className="relative ml-1.5 w-[1px] translate-y-2 bg-slate-200">
                 <div className="absolute top-0 right-1/2 h-1.5 w-1.5 translate-x-1/2 rounded-full bg-slate-200" />
               </div>
               <div className="pb-3">
                 <h3 className="font-semibold">Subquestion</h3>
                 <div className="mt-2">{chain.subquestion}</div>
-                <Result result={chain.result} />
+                <Result result={chain.result} loading={false} />
                 <h4 className="mt-3 font-medium">Contracted</h4>
-                <Result result={chain.contracted} />
+                <Result result={chain.contracted} loading={false} />
               </div>
             </div>
           );
