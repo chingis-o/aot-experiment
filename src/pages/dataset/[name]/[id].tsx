58f7d8f5c5d76b47dc0351ea098b1b1b1b7117fd
Chingis Ochirov <chingis09@gmail.com>
2025-04-28 20:22:04 +0800

    datasets are back

--- src/pages/dataset/[name]/[id].tsx
+++ src/pages/dataset/[name]/[id].tsx
@@ -4,6 +4,11 @@ import Question from "~/components/Question";
 import { useRouter } from "next/router";
 
 import gsm8k from "@/data/gsm8k/test.json";
+import bbh from "@/data/bbh/test.json";
+import hotpotqa from "@/data/hotpotqa/test.json";
+import longbench from "@/data/longbench/test.json";
+import math from "@/data/math/test.json";
+import mmlu from "@/data/mmlu/test.json";
 
 import type { Gsm8k } from "@/interfaces/datasets";
 
